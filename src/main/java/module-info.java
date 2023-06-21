ea0b0863e63c357b31723f8aecd810ab80da77bd
fzhnf <faizahnaf52@gmail.com>
2023-06-21 22:15:08 +0800

    revisi

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module tubespbo.emedicalrecords {
+module emr.consultationsheetapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
 
 
-    opens tubespbo.emedicalrecords to javafx.fxml;
-    exports tubespbo.emedicalrecords;
+    opens emr.consultationsheetapp to javafx.fxml;
+    exports emr.consultationsheetapp;
 }
